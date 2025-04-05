b0d062629a34bed6428bda012b21a86a945599e9
JasonLyy <7962578+JasonLyy@users.noreply.github.com>
2025-04-05 04:17:35 +0000

    setup VMs + talos

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,13 +13,28 @@ terraform {
       source  = "bpg/proxmox"
       version = "~> 0.72.0"
     }
+    talos = {
+      source  = "siderolabs/talos"
+      version = "~> 0.7.1"
+    }
   }
 }
 
 provider "proxmox" {
-  endpoint  = var.proxmox_api_url
-  api_token = var.proxmox_api_token
-  insecure  = true
+  endpoint = var.proxmox_api_url
+  insecure = true
+  username = var.proxmox_username
+  password = var.proxmox_password
+
+  # Unable to use api_token to work with siderolabs/talos
+
+  # endpoint  = var.proxmox_api_url
+  # api_token = var.proxmox_api_token
+  # insecure  = true
+  # ssh {
+  #   agent    = true
+  #   username = "terraform"
+  # }
 }
 
 variable "proxmox_api_token" {
@@ -30,5 +45,16 @@ variable "proxmox_api_token" {
 
 variable "proxmox_api_url" {
   description = "Proxmox API URL"
+  type        = string
+}
+
+variable "proxmox_username" {
+  description = "Proxmox Username"
+  type        = string
+  sensitive   = true
+}
+
+variable "proxmox_password" {
+  description = "Proxmox Password"
   type        = string
 }
